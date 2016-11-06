90af6f67e7c79f6d8c78cad8f83d44b24dff5a56
Jorge Escobar <j@jorge3.com>
2016-11-06 00:12:18 -0400

    finished 3.2

--- apps/3.2/counter/views.py
+++ apps/3.2/counter/views.py
@@ -1,5 +1,14 @@
-from app import app
+from app import app, db
+from counter.models import *
 
 @app.route('/')
 def init():
-    return "Hello, World"
+    counter = Counter.query.first()
+    if not counter:
+        counter = Counter(1)
+        db.session.add(counter)
+        db.session.commit()
+    else:
+        counter.count += 1
+        db.session.commit()
+    return "<h1>Counter: " + str(counter.count) + "</h1>"
